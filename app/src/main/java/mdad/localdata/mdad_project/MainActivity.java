1722eca6f9737ec9068c6d44bf854800eadd7522
MUHAMMAD DZULFIQAR BIN MD RASID 2005132B <2005132B@student.tp.edu.sg>
2023-01-26 18:48:22 +0800

    try fix jdk issue

--- app/src/main/java/mdad/localdata/mdad_project/MainActivity.java
+++ app/src/main/java/mdad/localdata/mdad_project/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnEx;
 
-    //dzul was here
+    //dzul was here tesadadsaddsadasd
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
