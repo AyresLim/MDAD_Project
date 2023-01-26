a2ead7cf9f03c816585eb8ede7b9aa945bbcb0ae
MUHAMMAD DZULFIQAR BIN MD RASID 2005132B <2005132B@student.tp.edu.sg>
2023-01-26 22:04:17 +0800

    added the rest of the genres
    have not put music

--- app/src/main/java/mdad/localdata/mdad_project/GenreActivity.java
+++ app/src/main/java/mdad/localdata/mdad_project/GenreActivity.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 public class GenreActivity extends AppCompatActivity {
 
     Button btnEDM;
+    Button btnMetal;
+    Button btnHiphop;
+    Button btnPop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,35 @@ public class GenreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_genre);
 
         btnEDM = (Button)findViewById(R.id.btnEdm);
+        btnMetal = (Button)findViewById(R.id.btnMetal);
+        btnHiphop = (Button)findViewById(R.id.btnHiphop);
+        btnPop = (Button)findViewById(R.id.btnPop);
 
         btnEDM.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(GenreActivity.this, EDMActivity.class));
+            }
+        });
+
+        btnMetal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GenreActivity.this, MetalActivity.class));
+            }
+        });
+
+        btnPop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GenreActivity.this, PopActivity.class));
+            }
+        });
+
+        btnHiphop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GenreActivity.this, HiphopActivity.class));
             }
         });
     }
