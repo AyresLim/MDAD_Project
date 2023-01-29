c8a02b64690fba87266c1255f0d6e3503a041931
MUHAMMAD DZULFIQAR BIN MD RASID 2005132B <2005132B@student.tp.edu.sg>
2023-01-30 01:29:02 +0800

    added edm fragments

--- app/src/main/java/mdad/localdata/mdad_project/EDMActivity.java
+++ app/src/main/java/mdad/localdata/mdad_project/EDMActivity.java
@@ -2,13 +2,60 @@ package mdad.localdata.mdad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import mdad.localdata.mdad_project.edmfragments.DarrenStylesFragment;
+import mdad.localdata.mdad_project.edmfragments.MarshmelloFragment;
+import mdad.localdata.mdad_project.edmfragments.MartinGarrixFragment;
+import mdad.localdata.mdad_project.edmfragments.WarriyoFragment;
 
 public class EDMActivity extends AppCompatActivity {
+
+    Button btnWarriyo;
+    Button btnMarshmello;
+    Button btnMG;
+    Button btnDS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edmactivity);
+
+        btnWarriyo = (Button)findViewById(R.id.btnWarriyo);
+        btnMarshmello = (Button)findViewById(R.id.btnMarshmello);
+        btnMG = (Button)findViewById(R.id.btnMG);
+        btnDS = (Button)findViewById(R.id.btnDS);
+
+        btnWarriyo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EDMActivity.this, WarriyoFragment.class));
+            }
+        });
+
+        btnMarshmello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EDMActivity.this, MarshmelloFragment.class));
+            }
+        });
+
+        btnMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EDMActivity.this, MartinGarrixFragment.class));
+            }
+        });
+
+        btnDS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EDMActivity.this, DarrenStylesFragment.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/mdad/localdata/mdad_project/edmfragments/WarriyoFragment.java
@@ -0,0 +1,69 @@
+package mdad.localdata.mdad_project.edmfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import mdad.localdata.mdad_project.R;
+
+public class WarriyoFragment extends AppCompatActivity {
+
+    public static ViewPager2 viewPager;
+
+    private FragmentStateAdapter pagerAdapter;
+
+    private String[] titles = new String[]{"Information", "Top Songs"};
+
+    private static final int NUM_PAGES = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warriyo_fragment);
+
+        viewPager = findViewById(R.id.mypager);
+
+        TabLayout tabLayout =(TabLayout) findViewById(R.id.tab_layout);
+
+        pagerAdapter = new MyPagerAdapter(this);
+
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayoutMediator tlm = new TabLayoutMediator(tabLayout,viewPager,(tab, position) -> tab.setText(titles[position]));
+
+        tlm.attach();
+    }
+
+    private class MyPagerAdapter extends FragmentStateAdapter {
+
+        public MyPagerAdapter(WarriyoFragment fa) {
+            super(fa);
+        }
+        @Override
+
+        public Fragment createFragment(int pos) {
+            switch (pos) {
+                case 0: {
+                    return FirstWarriyoFragment.newInstance("fragment 1",null);
+                }
+                case 1: {
+                    return SecondWarriyoFragment.newInstance("fragment 2",null);
+
+                }
+                default:
+                    return new Fragment();
+            }
+        }
+        @Override
+
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
+}
