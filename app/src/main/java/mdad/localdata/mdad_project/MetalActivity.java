33087f5f58ac63a9c90e2ac1c58025a0cba4b4d3
MUHAMMAD DZULFIQAR BIN MD RASID 2005132B <2005132B@student.tp.edu.sg>
2023-01-31 11:43:19 +0800

    added metal fragments

--- app/src/main/java/mdad/localdata/mdad_project/MetalActivity.java
+++ app/src/main/java/mdad/localdata/mdad_project/MetalActivity.java
@@ -2,13 +2,60 @@ package mdad.localdata.mdad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import mdad.localdata.mdad_project.metalfragments.BOFragment;
+import mdad.localdata.mdad_project.metalfragments.IPFragment;
+import mdad.localdata.mdad_project.metalfragments.MIWFragment;
+import mdad.localdata.mdad_project.metalfragments.SWSFragment;
 
 public class MetalActivity extends AppCompatActivity {
+
+    Button btnBO;
+    Button btnMIW;
+    Button btnSWS;
+    Button btnIP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metal);
+
+        btnBO = (Button)findViewById(R.id.btnBO);
+        btnIP = (Button)findViewById(R.id.btnIP);
+        btnMIW = (Button)findViewById(R.id.btnMIW);
+        btnSWS = (Button)findViewById(R.id.btnSWS);
+
+
+        btnBO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MetalActivity.this, BOFragment.class));
+            }
+        });
+
+        btnIP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MetalActivity.this, IPFragment.class));
+            }
+        });
+
+        btnMIW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MetalActivity.this, MIWFragment.class));
+            }
+        });
+
+        btnSWS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MetalActivity.this, SWSFragment.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/mdad/localdata/mdad_project/metalfragments/BOFragment.java
@@ -0,0 +1,69 @@
+package mdad.localdata.mdad_project.metalfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import mdad.localdata.mdad_project.R;
+
+public class BOFragment extends AppCompatActivity {
+
+    public static ViewPager2 viewPager;
+
+    private FragmentStateAdapter pagerAdapter;
+
+    private String[] titles = new String[]{"Information", "Top Songs"};
+
+    private static final int NUM_PAGES = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bofragment);
+
+        viewPager = findViewById(R.id.mypager);
+
+        TabLayout tabLayout =(TabLayout) findViewById(R.id.tab_layout);
+
+        pagerAdapter = new BOFragment.MyPagerAdapter(this);
+
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayoutMediator tlm = new TabLayoutMediator(tabLayout,viewPager,(tab, position) -> tab.setText(titles[position]));
+
+        tlm.attach();
+    }
+
+    private class MyPagerAdapter extends FragmentStateAdapter {
+
+        public MyPagerAdapter(BOFragment fa) {
+            super(fa);
+        }
+        @Override
+
+        public Fragment createFragment(int pos) {
+            switch (pos) {
+                case 0: {
+                    return FirstBOFragment.newInstance("fragment 1",null);
+                }
+                case 1: {
+                    return SecondBOFragment.newInstance("fragment 2",null);
+
+                }
+                default:
+                    return new Fragment();
+            }
+        }
+        @Override
+
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
+}
