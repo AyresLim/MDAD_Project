8332009d3ccebdfc10673d61064530651fea686a
MUHAMMAD DZULFIQAR BIN MD RASID 2005132B <2005132B@student.tp.edu.sg>
2023-02-06 19:20:19 +0800

    Added Info to warriyo songs and added urls to images (for songs)

--- app/src/main/java/mdad/localdata/mdad_project/warriyosongs/VenomActivity.java
+++ app/src/main/java/mdad/localdata/mdad_project/warriyosongs/VenomActivity.java
@@ -2,15 +2,112 @@ package mdad.localdata.mdad_project.warriyosongs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import mdad.localdata.mdad_project.R;
 
 public class VenomActivity extends AppCompatActivity {
 
+    TextView tvAbout;
+    ImageView imgSpotify;
+    ImageView imgApple;
+    ImageView imgDeezer;
+    ImageView imgYoutube;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venom);
+
+        imgSpotify = (ImageView) findViewById(R.id.imgSpotify);
+        imgApple = (ImageView) findViewById(R.id.imgApple);
+        imgDeezer = (ImageView) findViewById(R.id.imgDeezer);
+        imgYoutube = (ImageView) findViewById(R.id.imgYoutube);
+
+        tvAbout = (TextView) findViewById(R.id.tvAbout);
+        new VenomActivity.NetworkTask().execute("http://192.168.0.163/songs/warriyo/venom.txt");
+
+        imgSpotify.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Uri uri = Uri.parse("https://open.spotify.com/track/66pp2aKUQKW1vTY4sft0XS?autoplay=true");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        imgApple.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Uri uri = Uri.parse("https://music.apple.com/sg/album/venom/1584557903?i=1584557905");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        imgDeezer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Uri uri = Uri.parse("https://www.deezer.com/track/1486106932?autoplay=true");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        imgYoutube.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Uri uri = Uri.parse("https://music.youtube.com/watch?v=8jO5qRSZ6_s&feature=gws_kp_track");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+    private class NetworkTask extends AsyncTask<String, Void, String> {
+        String  testMsg="";
+        protected String doInBackground(String... params) {
+
+            try{
+
+                System.out.println("NetworkTask started……");
+                URL url = new URL(params[0]);
+
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestProperty("Accept", "text/plain");
+                BufferedReader br =new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                String line;
+                int i=0;
+                while((line=br.readLine())!=null) {
+                    testMsg+=line+"\n";
+                }
+                con.disconnect();
+            }
+            catch(IOException e){ Log.e("error", "error in reading", e); }
+            return testMsg;
+
+        }
+        protected void onPostExecute(String result) {
+            tvAbout.setText(result);
+
+        }
+    }
+
 }
+
